c2bd7af84db9a96b261399132810af5b7e19b747
minh duc <minhductxqt@gmail.com>
2022-07-20 15:15:34 +0700

    add list movies thumbnail

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Banner from "../components/Banner";
 import Header from "../components/Header";
 import requests from "../utils/requests";
 import { Movie } from "../typing";
+import Row from "../components/Row";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -25,7 +26,6 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -36,11 +36,15 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
         <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
